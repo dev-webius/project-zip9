7037f9ef715ba6e93496b2bd3f8a48430d098fa8
조민기 <jomg@jomgui-MacBookPro.local>
2023-07-18 17:12:59 +0900

    Test: OPEN API - DB 소요시간 테스트

--- server/src/test/java/com/zip9/api/announcement/service/AnnouncementDBServiceTest.java
+++ server/src/test/java/com/zip9/api/announcement/service/AnnouncementDBServiceTest.java
@@ -5,6 +5,7 @@ import com.zip9.api.announcement.repository.AnnouncementRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StopWatch;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,6 +17,8 @@ class AnnouncementDBServiceTest {
     @Autowired
     AnnouncementDBService announcementDBService;
 
+    @Autowired
+    AnnouncementOpenAPIService announcementOpenAPIService;
     @Autowired
     AnnouncementRepository announcementRepository;
 
@@ -50,5 +53,36 @@ class AnnouncementDBServiceTest {
                 .toList();
 
         assertTrue(true);
+    }
+
+    @Test
+    void open_api_vs_db_성능_테스트1() {
+        // given
+        AnnouncementRequest request = AnnouncementRequest.builder()
+                .registStartDate(LocalDate.parse("2023-07-01"))
+                .registEndDate(LocalDate.parse("2023-07-04"))
+                .build();
+
+        // when-1
+        StopWatch stopWatch1 = new StopWatch("OPEN_API_STOP_WATCH");
+        stopWatch1.start("[START] - getAnnouncements");
+        AnnouncementResponse r1 = announcementOpenAPIService.getAnnouncements(request);
+        stopWatch1.stop();
+
+        // when-2
+        StopWatch stopWatch2 = new StopWatch("DB_STOP_WATCH");
+        stopWatch2.start("[START] - getAnnouncements");
+        AnnouncementResponse r2 = announcementDBService.getAnnouncements(request);
+        stopWatch2.stop();
+
+        // then
+        printResult(stopWatch1);
+        printResult(stopWatch2);
+    }
+
+    private void printResult(StopWatch stopWatch1) {
+        System.out.println(stopWatch1.prettyPrint());
+        System.out.println("마지막 작업 걸린 시간 : " + stopWatch1.getLastTaskTimeNanos());
+        System.out.println("totalTimeSeconds : " + stopWatch1.getTotalTimeSeconds());
     }
 }
